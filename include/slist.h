c3cf1dca82b4d97508f306c232d27cbd394d3f00
Dr. Abiira Nathan <nabiira2by2@gmail.com>
2025-10-05 00:51:47 +0300

    Refactor Singly-Linked List implementation

--- include/slist.h
+++ include/slist.h
@@ -1,6 +1,7 @@
 #ifndef E3BD52A6_7A93_4758_8633_F1005A651260
 #define E3BD52A6_7A93_4758_8633_F1005A651260
 
+#include <stddef.h>
 #include <stdio.h>
 
 #if defined(__cplusplus)
@@ -9,41 +10,46 @@ extern "C" {
 
 // A node in a singly-linked list.
 typedef struct slist_node {
-    void* data;
+    void* data;  // pointer to element data (inline in node)
     struct slist_node* next;
 } slist_node_t;
 
 // A singly-linked list.
 typedef struct {
     slist_node_t* head;
+    slist_node_t* tail;  // tail pointer for O(1) append
     size_t size;
     size_t elem_size;
-} slist_t;
+} slist;
 
-slist_t* slist_new(size_t elem_size);
-void slist_free(slist_t* list);
+slist* slist_new(size_t elem_size);
+void slist_free(slist* list);
 
-size_t slist_size(slist_t* list);
-void slist_clear(slist_t* list);
+size_t slist_size(const slist* list);
+void slist_clear(slist* list);
 
 slist_node_t* slist_node_new(size_t elem_size, void* data);
 void slist_node_free(slist_node_t* node);
 
-void slist_push(slist_t* list, void* elem);
-void slist_pop(slist_t* list);
+void slist_push_front(slist* list, void* elem);
+void slist_push_back(slist* list, void* elem);
+void slist_pop_front(slist* list);
 
-void slist_insert(slist_t* list, size_t index, void* elem);
-void slist_remove(slist_t* list, size_t index);
+void slist_insert(slist* list, size_t index, void* elem);
+void slist_remove(slist* list, size_t index);
 
-void* slist_get(slist_t* list, size_t index);
-int slist_index_of(slist_t* list, void* elem);
+void* slist_get(const slist* list, size_t index);
+int slist_index_of(const slist* list, void* elem);
 
-void slist_insert_after(slist_t* list, void* elem, void* after);
+void slist_insert_after(slist* list, void* elem, void* after);
+void slist_insert_before(slist* list, void* elem, void* before);
 
-void slist_insert_before(slist_t* list, void* elem, void* before);
+void slist_print_asint(const slist* list);
+void slist_print_aschar(const slist* list);
 
-void slist_print_asint(slist_t* list);
-void slist_print_aschar(slist_t* list);
+// Iteration macro
+#define SLIST_FOR_EACH(list, node)                                                                 \
+    for (slist_node_t * (node) = (list)->head; (node); (node) = (node)->next)
 
 #if defined(__cplusplus)
 }
