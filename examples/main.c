a73ba5e763c992aeed6fd00d4c80cf856aec1a9c
Dr. Abiira Nathan <nabiira2by2@gmail.com>
2025-12-17 11:40:59 +0300

    Add example

---
+++ examples/main.c
@@ -0,0 +1,9 @@
+#include <solidc/cstr.h>
+
+int main() {
+    cstr* s = cstr_new("Hello World");
+    printf("String: %s\n", cstr_data_const(s));
+    cstr_free(s);
+}
+
+// cmake --build build && ./build/example
